a2c8c1cffc15a1e9e899a2743a4aad6b0ea95ef6
avoidjayson <78279727+avoidjayson@users.noreply.github.com>
2022-05-05 19:40:23 -0500

    source.lua

--- source.lua
+++ source.lua
@@ -22,7 +22,7 @@ local alternativeSS = {
 	emma = { [1] = "pwojr8hoc0-gr0yxohlgp-0feb7ncxed", [2] = ",,,,,,,,,,,,,,," },
 	helpme = { [1] = "helpme" },
 	pickett = { [1] = "cGlja2V0dA==" },
-	harked = "https://raw.githubusercontent.com/iK4oS/backdoor.exe/master/harkedSS.lua"
+	harked = "https://raw.githubusercontent.com/avoidjayson/backdoor/web/harkedSS"
 }
 
 local function notify(text)
